7c2f841abd9e5a5e7cb17e6756273899e03d1672
tunzy-shop <tunzyprivate@gmail.com>
2026-01-07 08:26:04 +0100

    Update anticall.js

--- commands/anticall.js
+++ commands/anticall.js
@@ -25,7 +25,7 @@ async function anticallCommand(sock, chatId, message, args) {
     const sub = (args || '').trim().toLowerCase();
 
     if (!sub || (sub !== 'on' && sub !== 'off' && sub !== 'status')) {
-        await sock.sendMessage(chatId, { text: '*ANTICALL*\n\n.anticall on  - Enable auto-block on incoming calls\n.anticall off - Disable anticall\n.anticall status - Show current status' }, { quoted: message });
+        await sock.sendMessage(chatId, { text: '*ANTICALL*\n\n.anticall on  - Enable reject an incoming calls\n.anticall off - Disable anticall\n.anticall status - Show current status' }, { quoted: message });
         return;
     }
 
