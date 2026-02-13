be363261bd546534381b9cb0137c45ccfabf24b1
tunzy-shop <tunzyprivate@gmail.com>
2026-02-13 19:25:26 +0100

    Add freefiresensiCommand import to main.js

--- main.js
+++ main.js
@@ -147,6 +147,7 @@ const { anticallCommand, readState: readAnticallState } = require('./commands/an
 const { pmblockerCommand, readState: readPmBlockerState } = require('./commands/pmblocker');
 const settingsCommand = require('./commands/settings');
 const soraCommand = require('./commands/sora');
+const freefiresensiCommand = require('./commands/freefiresensi');
 
 // Global settings
 global.packname = settings.packname;
