4f94f96d5a1743fa39d039ebca92071a84e39fbd
tunzy-shop <tunzyprivate@gmail.com>
2026-03-14 18:44:02 +0100

    Update vcf.js

--- commands/vcf.js
+++ commands/vcf.js
@@ -4,11 +4,15 @@ const path = require('path');
 
 async function vcfCommand(sock, chatId, message) {
     try {
+        if (!chatId.endsWith('@g.us')) {
+            return await sock.sendMessage(chatId, { text: '❌ This command can only be used in groups.' });
+        }
+
         const groupMetadata = await sock.groupMetadata(chatId);
         const participants = groupMetadata.participants;
         
         let vcfData = '';
-        participants.forEach((participant, index) => {
+        participants.forEach((participant) => {
             const name = participant.notify || participant.id.split('@')[0];
             const number = participant.id.split('@')[0];
             vcfData += `BEGIN:VCARD\nVERSION:3.0\nFN:${name}\nTEL;type=CELL:+${number}\nEND:VCARD\n`;
