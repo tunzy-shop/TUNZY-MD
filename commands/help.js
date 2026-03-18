52c2db74994ad0605442260b5907f4301d0a0ef9
tunzy-shop <tunzyprivate@gmail.com>
2026-03-18 22:04:09 +0100

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -32,7 +32,7 @@ async function helpCommandEdited(sock, chatId, message) {
     const caption = `
 ╭══〘 *TUNZY-MD* 〙══⊷
 ┃ *OH Hayoo :* ${userName}
-┃ *Version :* 1.0.0
+┃ *Version :* 2.0.0
 ┃ *Owner :* TUNZY SHOP
 ┃ *YouTube :* Tunzy Shop
 ╰═══════════════════════⊷
