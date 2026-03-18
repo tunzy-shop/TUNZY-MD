7102169ead05ce7b2d0f08abf405a17fff8c42ad
tunzy-shop <tunzyprivate@gmail.com>
2026-03-18 22:03:28 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -9,7 +9,7 @@ const settings = {
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
   description: "This is a bot for managing group commands and automating tasks.",
-  version: "1.0.0",
+  version: "2.0.0",
   updateZipUrl: "https://github.com/tunzy-shop/TUNZY-MD/archive/refs/heads/main.zip",
 };
 
